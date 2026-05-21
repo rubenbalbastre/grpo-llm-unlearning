5facdd9ee2de4220e52c48c3d23e81d6753a131f
rubenbalbastre <albalru.955@gmail.com>
2026-05-21 13:25:57 +0200

    placeholder function

---
+++ src/get_contaminated_data.py
@@ -0,0 +1,10 @@
+from datasets import load_dataset
+from typing import List
+
+
+def get_tofu_contaminated_data() -> List[str]:
+    pass
+
+
+def get_rwku_contaminated_data(topic: str) -> List[str]:
+    pass
