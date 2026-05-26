335b43b3fb9270b2d13ae2a76acdda7543395cf3
rubenbalbastre <albalru.955@gmail.com>
2026-05-26 11:36:32 +0200

    increase time limit

--- scripts/slurm-run-unlearning.sh
+++ scripts/slurm-run-unlearning.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name=slurm-run-unlearning
 #SBATCH --output=logs/slurm-run-unlearning-%j.log
 #SBATCH --gres=gpu:2
-#SBATCH --time=00:30:00
+#SBATCH --time=01:30:00
 #SBATCH --partition=sc-gpu
 set -euo pipefail
 
