2da19627825696eb5e765162291d84c01e20cc74
rubenbalbastre <albalru.955@gmail.com>
2026-06-19 14:43:39 +0200

    add script to run batch of experiments

---
+++ scripts/run-batch.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+authors=(
+  "Rihanna"
+  "Karl Marx"
+  # "Confucius"
+  "Nicolas Cage"
+  "Justin Timberlake"
+)
+
+for author in "${authors[@]}"; do
+  sbatch scripts/slurm-run-unlearning.sh "experiment.forget_concept=${author}"
+done
+
+for author in "${authors[@]}"; do
+  sbatch scripts/slurm-run-unlearning.sh "experiment.forget_concept=${author}" "training.mode=adaptive"
+done
+
+for author in "${authors[@]}"; do
+  sbatch scripts/slurm-run-unlearning.sh \
+    "experiment.forget_concept=${author}" \
+    "training.mode=adaptive" \
+    "data_generator.initial_standard_prompt_count=0"
+done
