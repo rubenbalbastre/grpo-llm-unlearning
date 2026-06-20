ab26595c1c4f3ceaca4808b053c694d8d6c2b3b4
rubenbalbastre <albalru.955@gmail.com>
2026-06-20 18:06:19 +0200

    add full R0, R1, R2 experiments

--- scripts/run-batch.sh
+++ scripts/run-batch.sh
@@ -1,27 +1,31 @@
 #!/usr/bin/env bash
 
 authors=(
-  "Rihanna"
+  # "Rihanna"
   # "Karl Marx"
-  # # # "Confucius"
+  "Confucius"
   # "Nicolas Cage"
   # "Justin Timberlake"
 )
 
-# for author in "${authors[@]}"; do
-#   sbatch scripts/slurm-run-unlearning.sh "experiment.forget_concept=${author}"
-# done
+# --------------------------------------------
+# 1st R0 runs:
+# --------------------------------------------
 
-# for author in "${authors[@]}"; do
-#   sbatch scripts/slurm-run-unlearning.sh "experiment.forget_concept=${author}" "training.mode=adaptive"
-# done
+for author in "${authors[@]}"; do
+  sbatch scripts/slurm-run-unlearning.sh "experiment.forget_concept=${author}"
+done
 
-# for author in "${authors[@]}"; do
-#   sbatch scripts/slurm-run-unlearning.sh \
-#     "experiment.forget_concept=${author}" \
-#     "training.mode=adaptive" \
-#     "data_generator.initial_standard_prompt_count=0"
-# done
+for author in "${authors[@]}"; do
+  sbatch scripts/slurm-run-unlearning.sh "experiment.forget_concept=${author}" "training.mode=adaptive"
+done
+
+for author in "${authors[@]}"; do
+  sbatch scripts/slurm-run-unlearning.sh \
+    "experiment.forget_concept=${author}" \
+    "training.mode=adaptive" \
+    "data_generator.initial_standard_prompt_count=0"
+done
 
 for author in "${authors[@]}"; do
   sbatch scripts/slurm-run-unlearning.sh \
@@ -29,4 +33,85 @@ for author in "${authors[@]}"; do
     "training.mode=adaptive" \
     "data_generator.data_proposer.objective=general" \
     "data_generator.initial_standard_prompt_count=0"
+done
+
+
+# --------------------------------------------
+# 2nd R1 runs:
+# --------------------------------------------
+
+authors=(
+  "Rihanna"
+  "Karl Marx"
+  "Confucius"
+  "Nicolas Cage"
+  "Justin Timberlake"
+)
+
+for author in "${authors[@]}"; do
+  sbatch scripts/slurm-run-unlearning.sh "experiment.forget_concept=${author}" \
+    "reward=r1"
+done
+
+for author in "${authors[@]}"; do
+  sbatch scripts/slurm-run-unlearning.sh "experiment.forget_concept=${author}" "training.mode=adaptive" \
+    "reward=r1"
+done
+
+for author in "${authors[@]}"; do
+  sbatch scripts/slurm-run-unlearning.sh \
+    "experiment.forget_concept=${author}" \
+    "training.mode=adaptive" \
+    "data_generator.initial_standard_prompt_count=0" \
+    "reward=r1"
+done
+
+for author in "${authors[@]}"; do
+  sbatch scripts/slurm-run-unlearning.sh \
+    "experiment.forget_concept=${author}" \
+    "training.mode=adaptive" \
+    "data_generator.data_proposer.objective=general" \
+    "data_generator.initial_standard_prompt_count=0" \
+    "reward=r1"
+done
+
+
+
+# --------------------------------------------
+# 3rd R2 runs:
+# --------------------------------------------
+
+authors=(
+  "Rihanna"
+  "Karl Marx"
+  "Confucius"
+  "Nicolas Cage"
+  "Justin Timberlake"
+)
+
+for author in "${authors[@]}"; do
+  sbatch scripts/slurm-run-unlearning.sh "experiment.forget_concept=${author}" \
+    "reward=r2"
+done
+
+for author in "${authors[@]}"; do
+  sbatch scripts/slurm-run-unlearning.sh "experiment.forget_concept=${author}" "training.mode=adaptive" \
+    "reward=r2"
+done
+
+for author in "${authors[@]}"; do
+  sbatch scripts/slurm-run-unlearning.sh \
+    "experiment.forget_concept=${author}" \
+    "training.mode=adaptive" \
+    "data_generator.initial_standard_prompt_count=0" \
+    "reward=r2"
+done
+
+for author in "${authors[@]}"; do
+  sbatch scripts/slurm-run-unlearning.sh \
+    "experiment.forget_concept=${author}" \
+    "training.mode=adaptive" \
+    "data_generator.data_proposer.objective=general" \
+    "data_generator.initial_standard_prompt_count=0" \
+    "reward=r2"
 done
