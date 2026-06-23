e128f3623647ade35d55f05e044d383b27ac3213
rubenbalbastre <albalru.955@gmail.com>
2026-06-23 18:24:40 +0200

    reduce experiment size

--- scripts/run-batch.sh
+++ scripts/run-batch.sh
@@ -1,33 +1,21 @@
 #!/usr/bin/env bash
 
-# authors=(
-#   # "Rihanna"
-#   # "Karl Marx"
-#   # "Confucius"
-#   # "Nicolas Cage"
-#   # "Justin Timberlake"
-# )
+authors=(
+  "Rihanna"
+  "Karl Marx"
+  # "Confucius"
+  "Nicolas Cage"
+  "Justin Timberlake"
+)
 
 # --------------------------------------------
 # 1st R0 runs:
 # --------------------------------------------
 
-# for author in "${authors[@]}"; do
-#   sbatch scripts/slurm-run-unlearning.sh "experiment.forget_concept=${author}"
-# done
+for author in "${authors[@]}"; do
+  sbatch scripts/slurm-run-unlearning.sh "experiment.forget_concept=${author}" "experiment.seed=12"
+done
 
-# for author in "${authors[@]}"; do
-#   sbatch scripts/slurm-run-unlearning.sh "experiment.forget_concept=${author}" "training.mode=adaptive" \
-#   "data_generator.data_proposer.objective=general"
-# done
-
-# for author in "${authors[@]}"; do
-#   sbatch scripts/slurm-run-unlearning.sh \
-#     "experiment.forget_concept=${author}" \
-#     "training.mode=adaptive" \
-#     "data_generator.data_proposer.objective=general" \
-#     "data_generator.initial_standard_prompt_count=0"
-# done
 
 # for author in "${authors[@]}"; do
 #   sbatch scripts/slurm-run-unlearning.sh \
@@ -42,90 +30,37 @@
 # 2nd R1 runs:
 # --------------------------------------------
 
-# authors=(
-#   # "Rihanna"
-#   # "Karl Marx"
-#   # "Confucius"
-#   # "Nicolas Cage"
-#   # "Justin Timberlake"
-# )
-
-# for author in "${authors[@]}"; do
-#   sbatch scripts/slurm-run-unlearning.sh "experiment.forget_concept=${author}" \
-#     "reward=r1"
-# done
-
-authors=(
-  "Rihanna"
-  "Karl Marx"
-  "Confucius"
-  "Nicolas Cage"
-  "Justin Timberlake"
-)
-
 for author in "${authors[@]}"; do
-  sbatch scripts/slurm-run-unlearning.sh "experiment.forget_concept=${author}" "training.mode=adaptive" \
+  sbatch scripts/slurm-run-unlearning.sh "experiment.forget_concept=${author}" \
     "reward=r1" \
-    "data_generator.data_proposer.objective=general"
-done
-
-for author in "${authors[@]}"; do
-  sbatch scripts/slurm-run-unlearning.sh \
-    "experiment.forget_concept=${author}" \
-    "training.mode=adaptive" \
-    "data_generator.initial_standard_prompt_count=0" \
-    "reward=r1" \
-    "data_generator.data_proposer.objective=general"
+    "experiment.seed=12"
 done
 
 # for author in "${authors[@]}"; do
 #   sbatch scripts/slurm-run-unlearning.sh \
 #     "experiment.forget_concept=${author}" \
 #     "training.mode=adaptive" \
-#     "data_generator.data_proposer.objective=general" \
 #     "data_generator.initial_standard_prompt_count=0" \
-#     "reward=r1"
+#     "reward=r1" \
+#     "data_generator.data_proposer.objective=general"
 # done
-
 
 
 # --------------------------------------------
 # 3rd R2 runs:
 # --------------------------------------------
 
-authors=(
-  "Rihanna"
-  "Karl Marx"
-  "Confucius"
-  "Nicolas Cage"
-  "Justin Timberlake"
-)
-
 for author in "${authors[@]}"; do
   sbatch scripts/slurm-run-unlearning.sh "experiment.forget_concept=${author}" \
-    "reward=r2"
-done
-
-for author in "${authors[@]}"; do
-  sbatch scripts/slurm-run-unlearning.sh "experiment.forget_concept=${author}" "training.mode=adaptive" \
-    "reward=r2" \
-    "data_generator.data_proposer.objective=general"
-done
-
-for author in "${authors[@]}"; do
-  sbatch scripts/slurm-run-unlearning.sh \
-    "experiment.forget_concept=${author}" \
-    "training.mode=adaptive" \
-    "data_generator.initial_standard_prompt_count=0" \
-    "reward=r2" \
-    "data_generator.data_proposer.objective=general"
+    "reward=r2"  \
+    "experiment.seed=12"
 done
 
 # for author in "${authors[@]}"; do
 #   sbatch scripts/slurm-run-unlearning.sh \
 #     "experiment.forget_concept=${author}" \
 #     "training.mode=adaptive" \
-#     "data_generator.data_proposer.objective=general" \
 #     "data_generator.initial_standard_prompt_count=0" \
-#     "reward=r2"
+#     "reward=r2" \
+#     "data_generator.data_proposer.objective=general"
 # done
