b823664ef1df50a263d781f9dc359a7d1501053f
rubenbalbastre <96421412+rubenbalbastre@users.noreply.github.com>
2026-07-28 11:53:14 +0200

    1 gpu

--- scripts/run-grpo.sh
+++ scripts/run-grpo.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -l
 #SBATCH --job-name=slurm-run-unlearning
 #SBATCH --output=logs/slurm-run-unlearning-%j.log
-#SBATCH --gres=gpu:2
+#SBATCH --gres=gpu:1
 #SBATCH --time=08:00:00
 #SBATCH --qos=hopper
 #SBATCH --partition=hopper
