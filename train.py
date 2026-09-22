9dfa58b218790d966001850dc6247b8773c2082e
rubenbalbastre <96421412+rubenbalbastre@users.noreply.github.com>
2026-09-22 15:55:04 +0200

    lr scheduler constant

--- train.py
+++ train.py
@@ -96,8 +96,8 @@ def main(cfg: DictConfig) -> None:
         # learning rate, scheduler
         learning_rate=cfg.training.grpo.learning_rate,
         lr_scheduler_type=cfg.training.grpo.lr_scheduler_type,
-        lr_scheduler_kwargs={"min_lr_rate": 0.1} if cfg.training.grpo.lr_scheduler_type == "cosine_with_min_lr" else None,
-        warmup_ratio=cfg.training.grpo.warmup_ratio,
+        # lr_scheduler_kwargs={"min_lr_rate": 0.1} if cfg.training.grpo.lr_scheduler_type == "cosine_with_min_lr" else None,
+        # warmup_ratio=cfg.training.grpo.warmup_ratio,
         # others
         num_iterations=cfg.training.grpo.num_iterations,
         beta=cfg.training.grpo.beta,
